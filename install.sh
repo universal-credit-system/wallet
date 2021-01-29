b944188da8c1021dae73075bce6f97808fdd8995
Universal Credit System <65252911+universal-credit-system@users.noreply.github.com>
2021-01-29 13:30:05 +0100

    Added lines for backup
    Added lines for backup functionality:
    mkdir ${script_path}/backup
    mkdir ${script_path}/backup/temp
    mkdir ${script_path}/backup/temp/keys
    mkdir ${script_path}/backup/temp/proofs
    mkdir ${script_path}/backup/temp/trx
    mkdir ${script_path}/backup/temp/control
    
    Added line for control-folder:
    mkdir ${script_path}/control

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 script_name=${0}
 script_path=$(dirname $(readlink -f ${0}))
-mkdir ${script_path}/keys
-mkdir ${script_path}/trx
-mkdir ${script_path}/proofs
 mkdir ${script_path}/backup
+mkdir ${script_path}/backup/temp
+mkdir ${script_path}/backup/temp/keys
+mkdir ${script_path}/backup/temp/proofs
+mkdir ${script_path}/backup/temp/trx
+mkdir ${script_path}/backup/temp/control
+mkdir ${script_path}/control
+mkdir ${script_path}/keys
+mkdir ${script_path}/proofs
+mkdir ${script_path}/trx
 chmod +x ${script_path}/ucs_client.sh
