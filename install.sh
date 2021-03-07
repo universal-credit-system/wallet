283368d3f7bc52960993866c068ec09f8eed7ebc
Universal Credit System <65252911+universal-credit-system@users.noreply.github.com>
2021-03-07 21:27:47 +0100

    Removed obsolete line for control/-folder
    Removed obsolete line for control/-folder

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ script_path=$(dirname $(readlink -f ${0}))
 umask 0022
 mkdir ${script_path}/backup
 mkdir ${script_path}/backup/temp
-mkdir ${script_path}/control
 mkdir ${script_path}/control/keys
 mkdir ${script_path}/keys
 mkdir ${script_path}/proofs
