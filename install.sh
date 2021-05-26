9464b2c8a0c918c98cfe271e372dc9cecf48ad76
Universal Credit System <65252911+universal-credit-system@users.noreply.github.com>
2021-05-26 18:06:24 +0200

    Logic for adding file and dir permissions
    Logic added to write file and dir permissions to config.conf

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-script_name=${0}
+
+###GET PATH#################
 script_path=$(dirname $(readlink -f ${0}))
+
+###CREATE DIRECTORIES#######
 mkdir ${script_path}/backup
-mkdir ${script_path}/backup/temp
 mkdir ${script_path}/control/keys
 mkdir ${script_path}/keys
 mkdir ${script_path}/proofs
 mkdir ${script_path}/trx
-mkdir ${script_path}/temp
-mkdir ${script_path}/temp/keys
-mkdir ${script_path}/temp/proofs
-mkdir ${script_path}/temp/trx
+mkdir ${script_path}/userdata
+
+###SAVE UMASK SETTINGS######
+user_umask=`umask`
+permissions_directories=`echo "777 - ${user_umask}"|bc`
+touch ${script_path}/test.tmp
+permissions_files=`stat -c '%a' ${script_path}/test.tmp`
+rm ${script_path}/test.tmp
+sed -i "s/permissions_directories=<permissions_directories>/permissions_directories=${permissions_directories}/g" ${script_path}/control/config.conf
+sed -i "s/permissions_files=<permissions_files>/permissions_files=${permissions_files}/g" ${script_path}/control/config.conf
