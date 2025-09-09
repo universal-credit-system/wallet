c1f6455280fb0b13ce3c05437e7bdc3d6cd0d895
Universal Credit System <65252911+universal-credit-system@users.noreply.github.com>
2025-09-09 13:38:34 +0200

    replace install.sh with auto_install.sh
    Changes:
    -automated detection of package management system
    -automated installation of required packages

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
 ### GET PATH #################
-script_path=$(dirname "$(readlink -f "${0}")")
+script_path=$(dirname $(readlink -f "${0}"))
+
+### GET ENVIRONMENT###########
+specific_env=$1
+specific_env=$(echo "${specific_env}"|tr '[A-Z]' '[a-z]')
+
+### USER TO INSTALL FOR#######
+specific_user=$2
+
+### SET VARIABLES ############
+error_detected=0
 
 ### CHECK DEPENDENCIES #######
-printf "%b" "INFO: Checking for dependencies..."
 while read program
 do
 	### CHECK IF PROGRAMM IS UNKNOWN ####
-        type "$program" >/dev/null
+        type "$program" >/dev/null 2>/dev/null
         rt_query=$?
         if [ $rt_query -gt 0 ]
         then
@@ -18,18 +27,108 @@ do
         				;;
         		"gpg")		echo "gnupg"  >>"${script_path}"/install_dep.tmp
         				;;
+        		"openssl")	if [ "${specific_env}" = "termux" ]
+        				then
+        					echo "openssl-tool"  >>"${script_path}"/install_dep.tmp
+        				else
+        					echo "${program}"  >>"${script_path}"/install_dep.tmp
+        				fi
+        				;;
         		*)		echo "${program}"  >>"${script_path}"/install_dep.tmp
         				;;
         	esac
         fi
 done <"${script_path}"/control/install.dep
-printf "%b" "DONE\n"
-
-if [ ! -s "${script_path}"/install_dep.tmp ]
-then	##############################
-	### IF ALL ARE INSTALLED #####
-	########### SETUP ############
-
+if [ -f ${script_path}/install_dep.tmp ] && [ -s ${script_path}/install_dep.tmp ]
+then
+	############################
+	###IF APPS ARE TO INSTALL###
+	###GET PACKAGE MANAGER######
+	case $specific_env in
+		"termux")	pkg_mngr="pkg"
+				;;
+		*)		pkg_mngr=""
+				if [ -x "$(command -v apk)" ]
+				then
+					pkg_mngr="apk";
+				else
+					if [ -x "$(command -v apt-get)" ]
+					then
+						pkg_mngr="apt-get";
+					else
+						if [ -x "$(command -v dnf)" ]
+						then
+							pkg_mngr="dnf";
+						else
+							if [ -x "$(command -v pacman)" ]
+							then
+								pkg_mngr="pacman";
+							else
+								if [ -x "$(command -v pkg)" ]
+								then
+									pkg_mngr="pkg";
+								else
+									if [ -x "$(command -v yum)" ]
+									then
+										pkg_mngr="yum";
+									else
+										if [ -x "$(command -v zypper)" ]
+										then
+											pkg_mngr="zypper";
+										else
+											###IF PACKAGING MANAGER DETECTION FAILED####
+											error_detected=1
+											no_of_programs=$(wc -l <${script_path}/install_dep.tmp)
+											echo "ERROR: Couldn't detect the package management system used on this machine!"
+											echo "Found ${no_of_programs} programs that need to be installed:"
+											cat ${script_path}/install_dep.tmp
+											echo "Install these programms first using your package management system and then run install.sh again."
+											############################################
+										fi
+									fi
+								fi
+							fi
+						fi
+					fi
+				fi
+				;;
+	esac
+	############################
+	
+	if [ -n "${pkg_mngr}" ] && [ $error_detected = 0 ]
+	then
+		### INSTALL MISSING PKGS #####
+		while read line
+		do
+			printf "%b" "INFO: Trying to install ${line} using ${pkg_mngr}...\n"
+			case $pkg_mngr in
+				"apk")		apk add $line ;;
+				"apt-get")	apt-get -y install $line ;;
+				"dnf")		dnf -y install $line ;;
+				"pacman")	pacman --noconfirm -S $line ;;
+				"pkg")		pkg install -y $line ;;
+				"yum")		yum -y install $line ;;
+				"zypper")	zypper -n install $line ;;
+			esac
+			rt_query=$?
+			if [ ! $rt_query = 0 ]
+			then
+				error_detected=1
+				echo "Error running the following command: ${pkg_mngr} install ${line}"
+				echo "Maybe the program ${line} is available in a package with different name."
+			fi
+		done <${script_path}/install_dep.tmp
+		############################
+	fi
+fi
+###REMOVE TMP FILE##########
+rm ${script_path}/install_dep.tmp 2>/dev/null
+if [ $error_detected = 0 ]
+then
+	if [ -n "${specific_user}" ]
+	then
+		su - "${specific_user}" || exit 1
+	fi
 	### CREATE DIRECTORIES #######
 	printf "%b" "INFO: Creating directories..."
 	mkdir -p "${script_path}"/backup
@@ -44,11 +143,11 @@ then	##############################
 	printf "%b" "INFO: Getting umask..."
 	user_umask=$(umask)
 	permissions_directories=$(echo "777 - ${user_umask}"|bc)
-	touch "${script_path}"/test.tmp
-	permissions_files=$(stat -c '%a' "${script_path}"/test.tmp)
-	rm "${script_path}"/test.tmp
+	touch ${script_path}/test.tmp
+	permissions_files=$(stat -c '%a' ${script_path}/test.tmp)
+	rm ${script_path}/test.tmp
 	printf "%b" "DONE\n"
-	
+
 	### IF OLD CONFIG THERE ######
 	if [ -s "${script_path}"/control/config.conf ]
 	then
@@ -56,15 +155,16 @@ then	##############################
 		mv "${script_path}"/control/config.conf "${script_path}"/control/config.bak
 		printf "%b" "DONE\n"
 	fi
-	
+
 	### COPY TO PLACE ############
 	printf "%b" "INFO: Copy install_config.conf to config.conf..."
-	cp "${script_path}"/control/install_config.conf "${script_path}"/control/config.conf
+	cp ${script_path}/control/install_config.conf ${script_path}/control/config.conf
 	printf "%b" "DONE\n"
-	
+
+	### WRITE PERMISSIONS ########
 	printf "%b" "INFO: Write umask to config.conf..."
-	sed -i "s/permissions_directories=permissions_directories/permissions_directories=${permissions_directories}/g" "${script_path}"/control/config.conf
-	sed -i "s/permissions_files=permissions_files/permissions_files=${permissions_files}/g" "${script_path}"/control/config.conf
+	sed -i "s/permissions_directories=permissions_directories/permissions_directories=${permissions_directories}/g" ${script_path}/control/config.conf
+	sed -i "s/permissions_files=permissions_files/permissions_files=${permissions_files}/g" ${script_path}/control/config.conf
 	printf "%b" "DONE\n"
 
 	### SET DEFAULT THEME ########
@@ -74,10 +174,10 @@ then	##############################
 
 	### SET PATHS ################
 	printf "%b" "INFO: Define paths in config.conf..."
-	sed -i "s#trx_path_input=trx_path_input#trx_path_input=${script_path}#g" "${script_path}"/control/config.conf
-	sed -i "s#trx_path_output=trx_path_output#trx_path_output=${script_path}#g" "${script_path}"/control/config.conf
-	sed -i "s#sync_path_input=sync_path_input#sync_path_input=${script_path}#g" "${script_path}"/control/config.conf
-	sed -i "s#sync_path_output=sync_path_output#sync_path_output=${script_path}#g" "${script_path}"/control/config.conf
+	sed -i "s#trx_path_input=trx_path_input#trx_path_input=${script_path}#g" ${script_path}/control/config.conf
+	sed -i "s#trx_path_output=trx_path_output#trx_path_output=${script_path}#g" ${script_path}/control/config.conf
+	sed -i "s#sync_path_input=sync_path_input#sync_path_input=${script_path}#g" ${script_path}/control/config.conf
+	sed -i "s#sync_path_output=sync_path_output#sync_path_output=${script_path}#g" ${script_path}/control/config.conf
 	printf "%b" "DONE\n"
 
 	### REWRITE CONFIG ###########
@@ -109,7 +209,6 @@ then	##############################
 		done <"${script_path}"/control/config.tmp
 		rm "${script_path}"/control/config.tmp
 	fi
-
 	### IF USER NEVER RAN GPG UNTIL NOW ######
 	if [ ! -d ~/.gnupg/ ]
 	then
@@ -118,7 +217,6 @@ then	##############################
 		gpg '?' 2>/dev/null
 		printf "%b" "DONE\n"
 	fi
-	### CONFIGURE gpg-agent.conf #############
 	if [ -s ~/.gnupg/gpg-agent.conf ]
 	then
 		printf "%b" "INFO: Checking gpg-agent.conf configuration..."
@@ -135,24 +233,11 @@ then	##############################
 		cp "${script_path}"/control/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 		printf "%b" "DONE\n"
 	fi
-	### USE KEYRING INSTEAD OF KEYBOX ########
 	if [ -s ~/.gnupg/common.conf ]
 	then
 		printf "%b" "INFO: Remove 'use-keyboxd' entry in ~/.gnupg/common.conf..."
 		sed -i 's/use-keyboxd//g' ~/.gnupg/common.conf
 		printf "%b" "DONE\n"
 	fi
-else
-	##############################
-	### IF APPS ARE TO INSTALL ###
-	########### ABORT ############
-
-	### DISPLAY APPS TO INSTALL ##
-	no_of_programs=$(wc -l <"${script_path}"/install_dep.tmp)
-        echo "INFO: Found ${no_of_programs} program(s) that need to be installed:"
-        awk '{print "INFO: -> " $1}' "${script_path}"/install_dep.tmp
-	echo "INFO: Install these programms first, then run install.sh again."
-
-	### REMOVE TMP FILE ##########
-        rm "${script_path}"/install_dep.tmp
 fi
+
